17e91d8633d45e91e512a1057908f41c018a2e61
Caladan8 <tnambiar2008@gmail.com>
2024-06-16 10:43:23 +0530

    Initial commit

---
+++ components.py
@@ -0,0 +1,46 @@
+import pygame
+from config import *
+import math
+import random
+
+class Player(pygame.sprite.Sprite):
+  def __init__(self, game):
+    self.game = game
+
+class Button:
+  def __init__(self, x, y, width, height, fg, bg, content, fontsize):
+    self.font = pygame.font.Font("EBGaramond.ttf", fontsize)
+
+    self.content = content
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.fg = fg
+    self.bg = bg
+    self.fontsize = fontsize
+    self.raw_bg_image = pygame.image.load("./images/ButtonBackgroundBrown.png")
+    self.bg_image = pygame.transform.scale(self.raw_bg_image, (self.width, self.height))
+
+    self.image = pygame.Surface((self.width, self.height), pygame.SRCALPHA, 32)
+    self.image.blit(self.bg_image, (0, 0))
+    self.rect = self.image.get_rect(center=(self.x, self.y))
+
+    self.text = self.font.render(self.content, True, self.fg)
+    self.text_coords = self.text.get_rect(center=(self.width / 2, self.height / 2))
+    self.image.blit(self.text, self.text_coords)
+  
+  def is_pressed(self, pos, pressed):
+    if self.rect.collidepoint(pos):
+      if pressed[0]:
+        return True
+      return False
+    return False
+
+  def is_hovering(self):
+    if self.rect.collidepoint(pygame.mouse.get_pos()):
+      return True
+  
+  def is_not_hovering(self):
+    if self.rect.collidepoint(pygame.mouse.get_pos()) == False:
+      return True
---
+++ main.py
@@ -0,0 +1,101 @@
+import pygame
+from pygame.locals import *
+from components import *
+from config import *
+
+class Game: 
+  def __init__(self):
+    pygame.init()
+    pygame.display.set_caption("Test")
+    self.screen = pygame.display.set_mode((800, 700), RESIZABLE)
+    self.font = pygame.font.Font('EBGaramond.ttf', 96)
+    self.running = True
+    self.raw_intro_background = pygame.image.load("./images/Background.jpg")
+    self.intro_background = pygame.transform.scale(self.raw_intro_background, (800, 700))
+    self.SCREEN_WIDTH, self.SCREEN_HEIGHT = pygame.display.get_surface().get_size()
+    self.window = self.screen.get_rect
+
+
+  def new(self):
+    # print("new() runs")
+    self.playing = True
+
+    # self.all_sprites = pygame.sprite.LayeredUpdates()
+    # self.blocks = pygame.sprite.LayeredUpdates()
+    # self.enemies = pygame.sprite.LayeredUpdates()
+    # self.attacks = pygame.sprite.LayeredUpdates()
+
+    # self.player = Player(self)
+
+  def events(self):
+    # print("event() running")
+    for event in pygame.event.get():
+      # print("pygame events: ", event.type, pygame.QUIT)
+      if event.type == pygame.QUIT:
+        self.playing = False
+        self.running = False
+      elif event.type == VIDEORESIZE:
+        self.screen.blit(pygame.transform.scale(self.intro_background, event.dict['size']), (0, 0))
+        self.SCREEN_WIDTH, self.SCREEN_HEIGHT = event.dict['size']
+    self.intro_screen()
+  
+  def draw(self):
+    # print("draw() running")
+    self.screen.fill(BLACK)
+    pygame.display.update()
+
+  def main(self):
+    # print("main() running", self.playing)
+    while self.playing:
+      self.events()
+      self.draw()
+    self.running = False
+
+  def intro_screen(self):
+    intro = True
+    # print("Intro screen running")
+    self.screen.blit(self.intro_background, (0, 0))
+
+
+    def show_display_text():
+      title = self.font.render("Welcome to Test", True, WHITE)
+      title_coords = title.get_rect(center=(self.SCREEN_WIDTH / 2, self.SCREEN_HEIGHT / 5))
+      self.screen.blit(title, title_coords)
+
+    while intro:
+      for event in pygame.event.get():
+        # print("pygame events: ", event.type, pygame.QUIT)
+        if event.type == pygame.QUIT:
+          intro = False
+          self.running = False
+          self.playing = False
+        elif event.type == VIDEORESIZE:
+          self.screen.blit(pygame.transform.scale(self.intro_background, event.dict['size']), (0, 0))
+          self.SCREEN_WIDTH, self.SCREEN_HEIGHT = event.dict['size']
+
+      play_button = Button(self.SCREEN_WIDTH / 2, self.SCREEN_HEIGHT / 2, 380, 120, WHITE, BROWN, 'PLAY', 24)
+
+      mouse_pos = pygame.mouse.get_pos()
+      mouse_pressed = pygame.mouse.get_pressed()
+
+      if play_button.is_pressed(mouse_pos, mouse_pressed):
+        intro = False
+      
+      if play_button.is_hovering():
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+      
+      if play_button.is_not_hovering():
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+        
+      show_display_text()
+
+      pygame.display.update()
+      self.screen.blit(play_button.image, play_button.rect)
+
+g = Game()
+g.new()
+
+while g.running:
+  g.main()
+
+pygame.quit()
