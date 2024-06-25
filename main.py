58a4dab40a471f243a8630d1543844d5c6490519
Caladan8 <tnambiar2008@gmail.com>
2024-06-25 07:06:04 +0530

    Add music

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from screens.intro_screen import *
 from screens.fight_screen import *
 from screens.victory_screen import *
 from screens.rest_screen import *
+from pygame import mixer 
 
 class Game: 
   def __init__(main):
@@ -42,22 +43,53 @@ class Game:
       elif event.type == VIDEORESIZE:
         main.screen.blit(pygame.transform.scale(main.intro_background, event.dict['size']), (0, 0))
         main.SCREEN_WIDTH, main.SCREEN_HEIGHT = event.dict['size']
+    
+    def play_intro_mus():
+      mixer.music.pause()      
+      mixer.music.load("./music/IntroMusic.mp3") 
+      mixer.music.set_volume(0.7) 
+      mixer.music.play() 
+    
+    def play_mob_mus():
+      mixer.music.pause()      
+      mixer.music.load("./music/MobMusic.mp3") 
+      mixer.music.set_volume(0.7) 
+      mixer.music.play() 
+
+    def play_boss_mus():
+      mixer.music.pause()      
+      mixer.music.load("./music/BossMusic.mp3") 
+      mixer.music.set_volume(0.7) 
+      mixer.music.play() 
+    
+    play_intro_mus()
     intro_screen(main)
 
+    
+    play_mob_mus()
     data = fight_screen(main, 40, "enemy_one")
+    
     if data[0]:
+      play_intro_mus()
       victory_screen(main, data)
+      play_mob_mus()
       data = fight_screen(main, 50, "enemy_two")
       if data[0]:
+        play_intro_mus()
         victory_screen(main, data)
         rest_screen(main)
+        play_mob_mus()
         data = fight_screen(main, 70, "enemy_three")
         if data[0]:
+          play_intro_mus()
           victory_screen(main, data)
+          play_mob_mus()
           data = fight_screen(main, 80, "enemy_four")
           if data[0]:
+            play_intro_mus()
             victory_screen(main, data)
             rest_screen(main),
+            play_boss_mus()
             data = fight_screen(main, 100, "boss")
     
     
